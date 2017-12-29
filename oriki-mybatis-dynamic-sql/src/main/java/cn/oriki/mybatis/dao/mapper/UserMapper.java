856e725d76106672221a7d0c245401508bec59cb
oriki <n0rk1n@live.cn>
2017-12-29 13:19:04 +0800

    4、动态SQL-if标签

--- oriki-mybatis-dynamic-sql/src/main/java/cn/oriki/mybatis/dao/mapper/UserMapper.java
+++ oriki-mybatis-dynamic-sql/src/main/java/cn/oriki/mybatis/dao/mapper/UserMapper.java
@@ -1,5 +1,27 @@
 package cn.oriki.mybatis.dao.mapper;
 
+import cn.oriki.mybatis.domain.User;
+
+import java.util.List;
+
 public interface UserMapper {
+    /**
+     * 根据用户名和邮箱查找用户
+     * 当只输入用户名时，根据用户名模糊查找
+     * 当只输入邮箱时，根据邮箱精准查找
+     * 既输入用户名又输入邮箱时，使用上述两个条件进行查找
+     *
+     * @param user
+     * @return
+     */
+    public List<User> queryUserByUsernameOrEmail(User user);
+
+    /**
+     * 根据id，选择性更新用户信息
+     *
+     * @param user
+     * @return
+     */
+    public Integer updateByIdSelective(User user);
 
 }
---
+++ oriki-mybatis-dynamic-sql/src/test/java/cn/oriki/mybatis/test/MyBatisTest.java
@@ -0,0 +1,49 @@
+package cn.oriki.mybatis.test;
+
+import cn.oriki.mybatis.dao.mapper.UserMapper;
+import cn.oriki.mybatis.domain.User;
+import cn.oriki.mybatis.utils.SqlSessionUtils;
+import org.apache.ibatis.session.SqlSession;
+import org.junit.Test;
+
+import java.util.List;
+
+public class MyBatisTest {
+    
+    @Test
+    public void test() {
+        SqlSession sqlSession = SqlSessionUtils.getSqlSession();
+        UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+
+
+        User user = new User();
+        user.setUsername("admi");// 模糊匹配
+        user.setEmail("admin@oriki.cn");
+
+        List<User> users = userMapper.queryUserByUsernameOrEmail(user);
+
+        for (User existUser : users) {
+            System.out.println(existUser);
+        }
+
+        sqlSession.close();
+    }
+
+    @Test
+    public void test2() {
+        SqlSession sqlSession = SqlSessionUtils.getSqlSession();
+        UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+
+        User user = new User();
+        user.setId(1);
+        user.setUsername("admin");// 模糊匹配
+        user.setEmail("admin@oriki.cn");
+
+        userMapper.updateByIdSelective(user);
+
+        sqlSession.rollback();
+
+        sqlSession.close();
+    }
+
+}
