a9dbabb214cd6508c7dfb892b736323f0795721f
oriki <n0rk1n@live.cn>
2017-12-29 12:53:07 +0800

    3.1、MyBatis 注解方式的基本用法-select查询

--- oriki-mybatis-annotation/src/main/java/cn/oriki/mybatis/dao/mapper/RoleMapper.java
+++ oriki-mybatis-annotation/src/main/java/cn/oriki/mybatis/dao/mapper/RoleMapper.java
@@ -1,5 +1,16 @@
 package cn.oriki.mybatis.dao.mapper;
 
+import cn.oriki.mybatis.domain.Role;
+import org.apache.ibatis.annotations.Select;
+
 public interface RoleMapper {
+    /**
+     * 根据id查询角色信息
+     *
+     * @param id
+     * @return
+     */
+    @Select(value = {"select r.id as id , r.name as name , r.enabled as enable , r.create_by as createBy , r.create_time as createTime from t_role r where id = #{id}"})
+    public Role queryById(Integer id);
 
 }
--- oriki-mybatis-annotation/src/test/java/cn/oriki/mybatis/test/MyBatisTest.java
+++ oriki-mybatis-annotation/src/test/java/cn/oriki/mybatis/test/MyBatisTest.java
@@ -1,5 +1,23 @@
 package cn.oriki.mybatis.test;
 
+import cn.oriki.mybatis.dao.mapper.RoleMapper;
+import cn.oriki.mybatis.domain.Role;
+import cn.oriki.mybatis.utils.SqlSessionUtils;
+import org.apache.ibatis.session.SqlSession;
+import org.junit.Test;
+
 public class MyBatisTest {
-    
+
+    @Test
+    public void test() {
+        SqlSession sqlSession = SqlSessionUtils.getSqlSession();
+        RoleMapper roleMapper = sqlSession.getMapper(RoleMapper.class);
+
+        Role role = roleMapper.queryById(1);
+
+        System.out.println(role);
+
+        sqlSession.close();
+    }
+
 }
